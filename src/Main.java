eca6c9f76c372a7a4f431fa141ae706465f0b40e
AygulSultan <jakypbaevaaygul@gmail.com>
2022-12-30 13:27:11 +0300

    comit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        //int []arr=new int[100];
+
+
+        System.out.println(Summ());
+    }
+    /*static void array(int[]arr) {
+        for (int i = 10; i<arr.length; i+=10) {
+            arr[i]=i;
+            if(arr[i]%3==0){
+                arr[i]=3333333;
+            }
+           arr[arr.length/2]=-1;
+            System.out.println(arr[i]+"");
+            System.out.println();
+
+
+        }*/
+
+        static int Summ()  {
+        int sum=0;
+        for(int i=0;i<300;i++){
+        if (i%3==0 @@ i%4==0 @@ i%5==0){
+        sum+=i}
+        return Summ;
+
+
+
+
+    }
+
+
+
+}
